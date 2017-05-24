67811c56694060c60000cfb5f5b6c1ef5c84229f
Andrey Mukamolow <fobo66@protonmail.com>
2017-05-24 22:45:58 +0300

    Implemented core library functionality

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,38 @@
-var functions = require('firebase-functions');
+// const functions = require('firebase-functions');
+// const admin = require('firebase-admin');
+// const algoliasearch = require('algoliasearch');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
+// admin.initializeApp(functions.config().firebase);
+// const algolia = algoliasearch(functions.config().algolia.app,
+//                               functions.config().algolia.key);
+// const index = algolia.initIndex(functions.config().algolia.index);
+
+const Promise = require('bluebird');
+
+const prepareObjectToExporting = (dataSnapshot) => {
+  const object = dataSnapshot.val();
+  object.objectID = dataSnapshot.key;
+  return object;
+};
+
+const updateExisting = (dataSnapshot, index) => {
+  const saveObject = Promise.promisify(index.saveObject, {
+    context: index,
+  });
+  return saveObject(prepareObjectToExporting(dataSnapshot));
+};
+
+const removeObject = (dataSnapshot, index) => {
+  const deleteObject = Promise.promisify(index.deleteObject, {
+    context: index,
+  });
+  return deleteObject(dataSnapshot.key);
+};
+
+exports.syncAlgoliaWithFirebase = (index, event) => {
+  if (!event.data.exists()) {
+    return removeObject(event.data, index);
+  }
+
+  return updateExisting(event.data, index);
+};

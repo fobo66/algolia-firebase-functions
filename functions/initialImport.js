9596057e600afaabda5c07729dd28008ee92389e
Andrey Mukamolow <fobo66@protonmail.com>
2017-05-24 17:56:50 +0300

    Script for initial importing made separated from functions

--- functions/initialImport.js
+++ functions/initialImport.js
@@ -1,26 +1,73 @@
-const functions = require('firebase-functions');
+#!/usr/bin/env node
+
+const argv = require('yargs')
+  .option('credential', {
+    alias: 'c',
+    description: 'Path to your service account key .json file',
+    default: './serviceAccountKey.json',
+    demandOption: 'Without this argument script cannot read your database',
+    type: 'string',
+  })
+  .option('database', {
+    alias: 'd',
+    description: 'Firebase Database URL',
+    demandOption: 'You should specify Firebase Database URL to import from',
+    type: 'string',
+  })
+  .option('key', {
+    alias: 'k',
+    description: 'Algolia app admin key',
+    demandOption: 'Script cannot write new items into Algolia app without this key. Use admin\'s key!',
+    type: 'string',
+  })
+  .option('app', {
+    alias: 'a',
+    description: 'Algolia App ID',
+    demandOption: 'You must specify Algolia app ID to import into',
+    type: 'string',
+  })
+  .option('ref', {
+    alias: 'r',
+    description: 'Firebase Database reference to import from',
+    demandOption: 'You must specify reference with data you want to import',
+    type: 'string',
+  })
+  .option('index', {
+    alias: 'i',
+    description: 'Algolia new index name',
+    demandOption: 'You must specify Algolia not already existing index name to import into',
+    type: 'string',
+  })
+  .argv;
+
 const admin = require('firebase-admin');
 const algoliasearch = require('algoliasearch');
+const serviceAccount = require(argv.credential);
 
-admin.initializeApp(functions.config().firebase);
-const algolia = algoliasearch(functions.config().algolia.app,
-                              functions.config().algolia.key);
-exports.initialImport = functions.https.onRequest((req, res) => {
-  if (req.method === 'PUT') {
-    res.status(403).send('Forbidden!');
-  }
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: argv.database
+});
+const algolia = algoliasearch(argv.app,
+                              argv.key);
 
-  cors(req, res,  () => {
-      const refToImport = admin.database().ref(req.body.ref);
-      const index = algolia.initIndex(req.body.index);
-      refToImport.once('value', (snapshot) => {
-        let objectsToIndex = [];
-        snapshot.forEach(child => {
-          let childKey = child.key;
-          let childData = child.val();
-          childData.objectID = childKey;
-          objectsToIndex.push(childData);
-        });
-      });
+const refToImport = admin.database().ref(argv.ref);
+const index = algolia.initIndex(argv.index);
+refToImport.once('value', (snapshot) => {
+  const objectsToIndex = [];
+  snapshot.forEach((child) => {
+    let childKey = child.key;
+    let childData = child.val();
+    childData.objectID = childKey;
+    objectsToIndex.push(childData);
+  });
+  index.saveObjects(objectsToIndex, (err, content) => {
+    if (!err) {
+      process.stdout.write('Firebase -> Algolia import done');
+      process.exit(0);
+    } else {
+      process.stderr.write('Something went wrong: ', err);
+      process.exit(1);
+    }
   });
 });

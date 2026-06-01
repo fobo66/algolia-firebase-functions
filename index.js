38bdbb1ba0299787aabca9618d4381a7ac7661c8
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2026-06-01 22:07:14 +0300

    Updated TS configs for v6 support

---
+++ index.js
@@ -0,0 +1,116 @@
+"use strict";
+//    Copyright 2017 Andrey Mukamolov <fobo66@protonmail.com>
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.syncAlgoliaWithFirebase = syncAlgoliaWithFirebase;
+exports.syncAlgoliaWithFirestore = syncAlgoliaWithFirestore;
+/**
+ * If a patch updates a nested object,
+ * it's necessary to parse it to an array
+ * @param dataVal - a JavaScript value from a DataSnapshot
+ * @returns if the object is nested or not
+ */
+// any is coming from Realtime Database
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const hasManyObjects = (dataVal) => {
+    const val = Object.values(dataVal);
+    return val[0] instanceof Object;
+};
+/**
+ * Forging object for uploading to Algolia
+ * Algolia requires "objectID" field in every object
+ * If not specified, it will generate it automatically
+ * To keep objects in sync, we specify objectID by ourselves
+ *
+ * @param id - Firebase Database key or Firestore id
+ * @param data - Child snapshot's data
+ */
+// any is coming from Realtime Database
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function prepareObjectToExporting(id, data) {
+    if (hasManyObjects(data)) {
+        return Object.entries(data).map((o) => ({
+            objectID: o[0],
+            ...o[1],
+        }));
+    }
+    const object = data;
+    object.objectID = id;
+    return [object];
+}
+/**
+ * Convenience wrapper over Algolia's SDK function for saving objects
+ *
+ * @param dataSnapshot - Child snapshot
+ * @param client - Algolia client
+ * @param index - Algolia index name
+ */
+async function updateExistingOrAddNewFirebaseDatabaseObject(dataSnapshot, client, index) {
+    return client.saveObjects({
+        indexName: index,
+        objects: prepareObjectToExporting(dataSnapshot.key ?? "", dataSnapshot.val()),
+    });
+}
+/**
+ * Convenience wrapper over Algolia's SDK function for saving objects
+ *
+ * @param dataSnapshot - Child snapshot
+ * @param client - Algolia client
+ * @param index - Algolia index name
+ */
+async function updateExistingOrAddNewFirestoreObject(dataSnapshot, client, index) {
+    return client.saveObjects({
+        indexName: index,
+        objects: prepareObjectToExporting(dataSnapshot.id, dataSnapshot.data()),
+    });
+}
+/**
+ * Convenience wrapper over Algolia's SDK function for deletion of the objects
+ *
+ * @param id - Firebase Database key or Firestore id
+ * @param client - Algolia client
+ * @param index - Algolia index name
+ */
+const removeObject = async (id, client, index) => client.deleteObject({
+    indexName: index,
+    objectID: id,
+});
+/**
+ * Determine whether it's deletion or update or insert action
+ * and send changes to Algolia
+ * Firebase Database Cloud Functions by default should return Promise object
+ * So, for usability, we return Promise too
+ * @param client - Algolia client
+ * @param index - Algolia index name
+ * @param change - Firebase Realtime database change
+ */
+async function syncAlgoliaWithFirebase(client, index, change) {
+    if (!change.after.exists()) {
+        return removeObject(change.before.key ?? "", client, index);
+    }
+    return updateExistingOrAddNewFirebaseDatabaseObject(change.after, client, index);
+}
+/**
+ * Determine whether it's deletion or update or insert action
+ * and send changes to Algolia
+ * @param client - Algolia client
+ * @param index - Algolia index name
+ * @param change - Firestore change
+ */
+async function syncAlgoliaWithFirestore(client, index, change) {
+    if (!change.after.exists) {
+        return removeObject(change.before.id, client, index);
+    }
+    return updateExistingOrAddNewFirestoreObject(change.after, client, index);
+}
